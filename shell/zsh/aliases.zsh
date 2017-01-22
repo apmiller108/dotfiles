8b6bf7cb0e71af9da75d73e07ad674c7aefd56db
Alex Miller <apmiller108@yahoo.com>
2017-01-22 10:47:16 -0500

    Add aliases and promot overrides

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -1,12 +1,33 @@
+#                              _ _                     
+#                         __ _| (_) __ _ ___  ___  ___ 
+#                        / _` | | |/ _` / __|/ _ \/ __|
+#                       | (_| | | | (_| \__ \  __/\__ \
+#                        \__,_|_|_|\__,_|___/\___||___/
+
+# Git
 alias g="git status"
 alias gaa="git add ."
 alias gc="git commit"
 alias gb="git branch"
 alias gco="git checkout"
+
+# Heroku toolbelt
 alias hrrc="heroku run rails c"
 alias hlpd="heroku local -f Procfile.dev"
-alias bex="bundle exec"
+
+# Docker
 alias dc="docker-compose"
 alias dcr="docker-compose run"
 alias cleandocker="sh ~/development/bash/clean-docker.sh"
+
+# Interactive and verbose for saftey
+
+alias cp="cp -iv"
+alias mv="mv -iv"
+alias rm="rm -iv"
+
+# The rest
 alias o="open ."
+alias bex="bundle exec"
+alias ls="ls -F"
+alias df="dh -h"
---
+++ shell/zsh/prompt.zsh
@@ -0,0 +1,14 @@
+#                                 _            _   _      
+# _ __  _ __ ___  _ __ ___  _ __ | |_ __ _ ___| |_(_) ___ 
+#| '_ \| '__/ _ \| '_ ` _ \| '_ \| __/ _` / __| __| |/ __|
+#| |_) | | | (_) | | | | | | |_) | || (_| \__ \ |_| | (__ 
+#| .__/|_|  \___/|_| |_| |_| .__/ \__\__,_|___/\__|_|\___|
+#|_|                       |_|                            
+
+# comes from the miloshadzic theme
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$reset_color%}%{$fg[green]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[yellow]%}⚡%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
+
+PROMPT='%{$fg[cyan]%}%2~%{$reset_color%}%{$fg[red]%}|%{$reset_color%}$(git_prompt_info)%{$fg[cyan]%}⇒%{$reset_color%} '
