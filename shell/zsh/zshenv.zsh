b890ace738cfd54cd32144392bcf8695d56e250a
Alex Miller <apmiller108@yahoo.com>
2019-07-28 13:18:49 -0400

    Fix missing qt

--- shell/zsh/zshenv.zsh
+++ shell/zsh/zshenv.zsh
@@ -15,6 +15,7 @@ export PATH="/Users/upgraydd/anaconda/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# export PATH="$(brew --prefix qt@5.5)/bin:$PATH"
 # For rbenv shims
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
