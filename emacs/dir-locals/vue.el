89a9bcef8e963f4f44b9a439e290c7616d2ce7ad
Alex Miller <apmiller108@yahoo.com>
2020-06-10 22:10:05 -0400

    Dynamically set url for chrome debugging

---
+++ emacs/dir-locals/vue.el
@@ -0,0 +1,7 @@
+((js2-mode
+  (flycheck-checker . javascript-eslint)
+  (eval . (setq-local flycheck-javascript-eslint-executable
+                      (concat (locate-dominating-file default-directory ".dir-locals.el")
+                              "node_modules/.bin/eslint"))))
+ (nil
+  (eval . (setq-local my/dap-debug-url "http://localhost:8080"))))
