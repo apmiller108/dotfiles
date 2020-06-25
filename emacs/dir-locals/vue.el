0bca32fc99bda210c6d16b0dcf9d18f13758eb97
Alex Miller <apmiller@covermymeds.com>
2020-06-25 14:03:45 -0400

    Sets eslint executable for vue-mode

--- emacs/dir-locals/vue.el
+++ emacs/dir-locals/vue.el
@@ -3,5 +3,10 @@
   (eval . (setq-local flycheck-javascript-eslint-executable
                       (concat (locate-dominating-file default-directory ".dir-locals.el")
                               "node_modules/.bin/eslint"))))
+ (vue-mode
+  (flycheck-checker . javascript-eslint)
+  (eval . (setq-local flycheck-javascript-eslint-executable
+                      (concat (locate-dominating-file default-directory ".dir-locals.el")
+                              "node_modules/.bin/eslint"))))
  (nil
   (eval . (setq-local my/dap-debug-url "http://localhost:8080"))))
