ce71e8f46a6479a4f8319c23869109b0e43a501f
Alex Miller <apmiller108@yahoo.com>
2021-09-20 21:01:25 -0500

    Updates env to work on Ubuntu and MacOS

--- shell/zsh/zshenv.zsh
+++ shell/zsh/zshenv.zsh
@@ -1,14 +1,29 @@
 export PATH=/usr/local/bin:$PATH
-export PATH="$HOME/.npm-packages/bin:$PATH" # for npm and npm global packages
-export PATH="$PATH:`yarn global bin`" # for Yarn for global packages
+
+# NPM and Yarn
+if command -v npm &> /dev/null
+then 
+  export PATH="$HOME/.npm-packages/bin:$PATH" # for npm and npm global packages
+fi
+
+if command -v yarn &> /dev/null
+then
+  export PATH="$PATH:`yarn global bin`" # for Yarn for global packages
+fi
+
 export PATH="$HOME/.emacs.d/bin:$PATH" # for Doom emacs
-PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH" # for tar
+
 # NVM
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # For rbenv shims
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+if command -v rbenv &> /dev/null
+then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+fi
+
 # iterm2 shell integration
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
