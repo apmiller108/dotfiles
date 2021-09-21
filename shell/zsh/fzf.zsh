7e1a6506647f1bdf692662e2a109d0b348144af5
Alex Miller <apmiller108@yahoo.com>
2021-09-20 21:00:41 -0500

    Adds checks for key binding file location

--- shell/zsh/fzf.zsh
+++ shell/zsh/fzf.zsh
@@ -1,13 +1,23 @@
+# fzf is a general-purpose command-line fuzzy finder.
+# https://github.com/junegunn/fzf
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
+# Auto-completion and Key bindings
+# --------------------------------
+
+# MacOS
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
 
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+if test -f "/usr/local/opt/fzf/shell/key-bindings.zsh"; then
+  source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+fi
+
+# Ubuntu
+if test -f "/usr/share/doc/fzf/examples/key-bindings.zsh"; then
+  source "/usr/share/doc/fzf/examples/key-bindings.zsh"
+  source "/usr/share/doc/fzf/examples/completion.zsh"
+fi
