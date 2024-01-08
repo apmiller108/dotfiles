de58b7c8721980fe5b9c1559a11fc20611ba135c
Alex Miller <apmiller@covermymeds.com>
2024-01-08 15:51:04 -0600

    Cleans up and adds aliases

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -12,34 +12,25 @@ alias gb="git branch | cat"
 alias gco="git checkout"
 alias gl="git log --graph --pretty=format:\"%C(yellow)%h%C(reset) %G? %C(bold blue)%an%C(reset) %s%C(bold cyan)%d%C(reset) %C(green)%cr.%C(reset)\""
 
-# Heroku toolbelt
-alias hrrc="heroku run rails c"
-alias hlpd="heroku local -f Procfile.dev"
-alias he="heroku"
-alias hr="heroku run"
-
 # Docker
-alias dc="docker compose"
 alias de='docker exec -e COLUMNS="$(tput cols)" -e LINES="$(tput lines)" -it'
 alias dgo='docker run -e COLUMNS="$(tput cols)" -e LINES="$(tput lines)" -it --rm -p 4567:4567 -v "$PWD":/app -w "/app"'
 alias dgoruby3='dgo ruby:3 bash'
 alias da='docker attach'
 alias dps='docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}\t{{.Command}}\t{{.Image}}"'
 alias dep='de postgres psql -U admin'
-alias dl='docker logs'
-alias dstartall='docker start $(docker ps -a -q -f status=exited)'
 alias dstopall='docker stop $(docker ps -aq)'
+
+# Docker Compose
+alias dc="docker compose"
+
+# HTTP server
 alias dserve='docker run --rm -p 8080:80 --name="myapache" -v "$PWD":/usr/local/apache2/htdocs/ httpd'
 
 # Interactive and verbose for saftey
 alias cp="cp -iv"
 alias mv="mv -iv"
 alias rm="rm -iv"
-
-# Elixir and Phoenix
-alias el="elixir"
-alias mps="mix phoenix.server"
-alias ism="iex -S mix"
 
 # Emacs
 alias spacemacsbg="emacs --with-profile spacemacs --daemon=spacemacs"
@@ -57,3 +48,10 @@ alias ls="ls -F"
 alias ngrok="~/ngrok"
 alias myaliases="cat ~/dotfiles/shell/zsh/aliases.zsh"
 alias sourcez="source ~/.zshrc"
+
+# Project dev
+
+# TMP
+alias twd="dc exec -it web"
+alias twt="dc exec web rspec"
+alias tdd="dc exec -it database psql -U postgres -d tmp_development"
